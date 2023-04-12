7dda5a88f862d5985366194a4126b2b78b4be529
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-04-12 07:30:02 -0400

    Connect API to search bar input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ function SeachBar( {onSubmit} ) {
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
-        onSubmit('cars');
+        onSubmit(term);
     }
     
     const handleChange = (event) => {
